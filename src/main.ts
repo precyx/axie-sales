49a692d5ff13ff3c016585a5536039c92552e62b
pquantum <pr.pascal.bachmann@gmail.com>
2018-03-29 22:14:46 +0200

    add log detail view

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,9 @@ window.addEventListener('load', function() {
 
   //window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
   //window.web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-  window.web3 = new Web3(window.web3.currentProvider || new Web3.providers.HttpProvider("https://mainnet.infura.io/kMogkVTqBa2OEdnAip8D"));
+  //window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+  window.web3 = new Web3(window.web3.currentProvider || new Web3.providers.HttpProvider('http://localhost:8545') || new Web3.providers.HttpProvider("https://mainnet.infura.io/kMogkVTqBa2OEdnAip8D"));
+  //window.web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/kMogkVTqBa2OEdnAip8D") || window.web3.currentProvider);
   // || Web3.givenProvider
   //console.log(Web3);
 
